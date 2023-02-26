dfe1e40f4f90f4ebfeefaf1c0e8468dcdd264f08
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2023-02-26 20:15:15 +0700

    feat(broker): read and write json

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"encoding/json"
-	"net/http"
-)
-
-type jsonResp struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
+import "net/http"
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResp{
@@ -17,9 +8,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broker",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	_ = app.writeJson(w, http.StatusOK, payload)
 }
