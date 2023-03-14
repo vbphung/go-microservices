beb804e8c30276091c109eba9ad19270f2517bd4
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2023-03-14 10:41:30 +0700

    feat: init mail service

---
+++ mail-service/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Config struct{}
+
+const webPort = "80"
+
+func main() {
+	app := Config{}
+
+	log.Println("Start mail service: ", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panicln("Cannot start server: ", err)
+	}
+}
