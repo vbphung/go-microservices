27e968ee46151e5f88974e69f7a6a29a3e3f2ae4
jimmyvo0512 <jimmyvo.05122002@gmail.com>
2023-03-17 21:52:17 +0700

    feat: add consumer and event

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,103 @@
+package event
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	csm := Consumer{
+		conn: conn,
+	}
+
+	err := csm.setup()
+	if err != nil {
+		return Consumer{}, err
+	}
+
+	return csm, nil
+}
+
+func (csm *Consumer) Listen(topics []string) error {
+	ch, err := csm.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, t := range topics {
+		err = ch.QueueBind(q.Name, t, "logs_topic", false, nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+
+	fr := make(chan bool)
+	go func() {
+		for m := range msgs {
+			var pl Payload
+			_ = json.Unmarshal(m.Body, &pl)
+
+			go handlePayload(pl)
+		}
+	}()
+
+	fmt.Printf("Waiting for msg [logs_topic, %s]", q.Name)
+
+	<-fr
+
+	return nil
+}
+
+func handlePayload(pl Payload) {
+	switch pl.Name {
+	case "auth":
+	case "log", "event":
+		err := logEvent(pl)
+		if err != nil {
+			log.Println(err)
+		}
+	default:
+		err := logEvent(pl)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func logEvent(pl Payload) error {
+	return nil
+}
+
+func (csm *Consumer) setup() error {
+	ch, err := csm.conn.Channel()
+	if err != nil {
+		return err
+	}
+
+	return declareExchange(ch)
+}
